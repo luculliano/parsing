c95e1f568ac95c371746acb4db4f2ed8032160c2
luculliano <vederecento@gmail.com>
2023-04-16 23:47:30 +0500

    first echo bot with requests

--- scraping/telegrambotapi3.py
+++ scraping/telegrambotapi3.py
@@ -1,21 +1,28 @@
-from telegrambotapi2 import getUpdates, sendMessage
+"""echo bot"""
+import os
 import time
 
+from telegrambotapi2 import getUpdates, sendMessage
+
 if __name__ == "__main__":
-    token = ""
-    counter = 0
+    token = os.getenv("BOT_TOKEN")
+    if token is None:
+        exit()
+    counter = 1
     offset = -2
-    while counter < 20:
-        print(counter)
+    while counter <= 20:
         # as the first offset will be -1, bellow a get the value
         # and increase it + 1 too
         updates = getUpdates(
             f"https://api.telegram.org/bot{token}/getUpdates?offset={offset + 1}"
         )
+        print("query for updates", counter)
         if updates["result"]:
             offset = updates["result"][0]["update_id"]
             chat_id = updates["result"][0]["message"]["from"]["id"]
-            sendMessage(chat_id, "Hello!", token)
-        # get query to server for getUpdates each 2 second
-        time.sleep(2)
+            name = updates["result"][0]["message"]["from"]["username"]
+            print("replying...")
+            sendMessage(chat_id, f"Hello, {name}!", token)
+        # get query to server for getUpdates each second
+        time.sleep(1)
         counter += 1
