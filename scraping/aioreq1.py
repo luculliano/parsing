6e39cef666be820ef6a22f12ecd89a195a45c52d
luculliano <vederecento@gmail.com>
2023-04-16 12:42:55 +0500

    modify aireq1, req1

--- scraping/aioreq1.py
+++ scraping/aioreq1.py
@@ -1,52 +1,63 @@
 import asyncio
-from ctypes import resize
 import json
-from time import monotonic
 
 import aiofiles
 import aiohttp
 
+
 ###############################################################################
 # simple get query
-# async def main() -> None:
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(
-#             "https://api.covidtracking.com/v1/us/current.json"
-#         ) as response:
-#             text = await response.json()
-#             print(json.dumps(*text, indent=2))
+async def main1() -> None:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://api.covidtracking.com/v1/us/current.json"
+        ) as response:
+            text = await response.json()
+            print(json.dumps(*text, indent=2))
 
 
-# asyncio.run(main())
+asyncio.run(main1())
+
+
 ###############################################################################
 # using proxy
-# async def main() -> None:
-#     headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0"}
-#     proxy = "http://72bb24:ed1aea@87.251.76.222:1010"
-#     # proxy_auth = aiohttp.BasicAuth(login="72bb24", password="ed1aea")
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get("https://ipinfo.io/json",
-#                                proxy=proxy,
-#                                # proxy_auth=proxy_auth
-#                                headers=headers) as response:
-#             print(await response.text())
+async def main2() -> None:
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0"
+    }
+    proxy = "http://72bb24:ed1aea@87.251.76.222:1010"
+    # proxy_auth = aiohttp.BasicAuth(login="72bb24", password="ed1aea")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://ipinfo.io/json",
+            proxy=proxy,
+            # proxy_auth=proxy_auth
+            headers=headers,
+        ) as response:
+            print(await response.text())
 
 
-# asyncio.run(main())
+asyncio.run(main2())
+
+
 ###############################################################################
 # without context manager
-# async def main() -> str:
-#     session = aiohttp.ClientSession()
-#     response = await session.get("https://httpbin.org/user-agent")
-#     await session.close()
-#     return await response.text()
-# print(asyncio.run(main()))
+async def main3() -> str:
+    session = aiohttp.ClientSession()
+    response = await session.get("https://httpbin.org/user-agent")
+    await session.close()
+    return await response.text()
+
+
+print(asyncio.run(main3()))
+
+
 ###############################################################################
 # read file with aiofiles.open function
-# async def main() -> None:
-#     async with aiofiles.open("proxies.txt", encoding="utf-8") as file:
-#         print(await file.read())
+async def main4() -> None:
+    async with aiofiles.open("proxies.txt", encoding="utf-8") as file:
+        print(await file.read())
 
 
-# asyncio.run(main())
+asyncio.run(main4())
 ###############################################################################
--- scraping/req1.py
+++ scraping/req1.py
@@ -1,41 +1,45 @@
 import json
 import os
 import sys
-from time import monotonic
 
 import requests
 
 ###############################################################################
 # request json get
-# res = requests.get("https://api.covidtracking.com/v1/us/current.json")
-# json.dump(*res.json(), fp=sys.stdout, indent=2)
+res = requests.get("https://api.covidtracking.com/v1/us/current.json")
+json.dump(*res.json(), fp=sys.stdout, indent=2)
 
 ###############################################################################
 # using params for API
-# api_key = os.getenv("OWM_API")
-# url = "https://api.openweathermap.org/data/2.5/weather"
-# params = {"appid": api_key, "lat": 44.34, "lon": 10.99}
-# res = requests.get(url=url, params=params)
-# print(res.url)
+api_key = os.getenv("OWM_API")
+url = "https://api.openweathermap.org/data/2.5/weather"
+params = {"appid": api_key, "lat": 44.34, "lon": 10.99}
+res = requests.get(url=url, params=params)
+print(res.url)
 ###############################################################################
 # download file
-# url = "https://httpbin.org/image/jpeg"
-# res = requests.get(url=url)
-# with open(f"downloaded.jpeg", "wb") as file:
-#     file.write(res.content)
+url = "https://httpbin.org/image/jpeg"
+res = requests.get(url=url)
+with open(f"downloaded.jpeg", "wb") as file:
+    file.write(res.content)
 
 # download large file
-# url = "https://user-images.githubusercontent.com/1991296/224442907-7693d4be-acaa-4e01-8b4f-add84093ffff.mp4"
-# res = requests.get(url=url, stream=True)
-# with open("video.mp4", "wb") as file:
-#     for chunk in res.iter_content(chunk_size=1_000_000):
-#         if chunk:
-#             file.write(chunk)
+url = "https://user-images.githubusercontent.com/1991296/224442907-7693d4be-acaa-4e01-8b4f-add84093ffff.mp4"
+res = requests.get(url=url, stream=True)
+with open("video.mp4", "wb") as file:
+    for chunk in res.iter_content(chunk_size=1_000_000):
+        if chunk:
+            file.write(chunk)
 ###############################################################################
 # using proxy
-# headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0"}
-# proxy = {"http": "http://72bb24:ed1aea@87.251.76.222:1010", "https": "http://72bb24:ed1aea@87.251.76.222:1010"}
-# url = "https://httpbin.org/user-agent"
-# res = requests.get("https://ipinfo.io/json",  proxies=proxy, headers=headers)
-# print(res.text)
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0"
+}
+proxy = {
+    "http": "http://72bb24:ed1aea@87.251.76.222:1010",
+    "https": "http://72bb24:ed1aea@87.251.76.222:1010",
+}
+url = "https://httpbin.org/user-agent"
+res = requests.get("https://ipinfo.io/json", proxies=proxy, headers=headers)
+print(res.text)
 ###############################################################################
