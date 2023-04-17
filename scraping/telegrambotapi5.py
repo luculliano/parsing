7439876884750a86e088e565277238cddb6d1295
luculliano <vederecento@gmail.com>
2023-04-17 18:28:55 +0500

    start typed_python and write picture bot

--- scraping/telegrambotapi5.py
+++ scraping/telegrambotapi5.py
@@ -1,73 +1,86 @@
-"""bot that sends pictures"""
-from typing import Any
-import aiohttp
 import asyncio
-import os
 import logging
+import os
 import sys
+from typing import Any
 
+import aiohttp
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(funcName)s",
+    format="%(asctime)s; %(levelname)s; %(funcName)s",
     stream=sys.stdout,
     datefmt="%d.%m.%Y, %H:%M:%S",
 )
 
 
 class PictureBot:
-    async def __init__(self) -> None:
+    """bot that sends pictures"""
+
+    def __init__(self) -> None:
         self.__TOKEN = os.getenv("TOKEN_BOT")
+        self.offset = -2
 
-    async def _get_picture(self) -> str:
+    async def _get_photo(self) -> str | None:
         """get picture from third-party api"""
+
+        url = "https://api.thecatapi.com/v1/images/search"
         async with aiohttp.ClientSession() as session:
-            async with session.get(
-                "https://api.thecatapi.com/v1/images/search"
-            ) as response:
+            async with session.get(url) as response:
                 logging.info("")
-                result = await response.json()
-                return result[0]["url"]
-
-
-    async def getUpdates(self, offset: int) -> dict[Any, Any] | None:
-        url = f"https://api.telegram.org/bot{self.__TOKEN}/getUpdates"
-        params = {"offset": offset + 1}
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params) as response:
-                updates = await response.json()
-                logging.info("get updates")
+                # content type=None to avoid aiohttp.client_exceptions.ContentTypeError
                 try:
-                    return updates["result"][0]
-                except IndexError:
-                    return None
+                    result = await response.json()
+                    return result[0]["url"]
+                except Exception:
+                    pass
 
+    async def getUpdates(self, session: aiohttp.ClientSession) -> list[Any]:
+        """check queries/updates from telegram server to bot as long pooling"""
+
+        url = f"https://api.telegram.org/bot{self.__TOKEN}/getUpdates"
+        params = {"offset": self.offset + 1, "timeout": 60}
+        async with session.get(url, params=params) as response:
+            logging.info("")
+            updates = await response.json()
+            return updates["result"]
 
     async def _parseUpdates(self, result: dict[Any, Any]) -> tuple[int, int]:
+        """export offset and chat_id from updates"""
+
         offset = result["update_id"]
         chat_id = result["message"]["from"]["id"]
         return offset, chat_id
 
+    async def sendPhoto(self, chat_id: int, session: aiohttp.ClientSession) -> None:
+        """reply as picture from queries to bot"""
 
-    async def sendPhoto(self, chat_id: int, photo_link: str) -> None:
-        url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
-        params = {"chat_id": chat_id, "photo": photo_link}
-        async with aiohttp.ClientSession() as session:
+        url = f"https://api.telegram.org/bot{self.__TOKEN}/sendPhoto"
+        photo_link = await self._get_photo()
+        if photo_link is None:
+            await self.sendMessage(chat_id, session)
+        else:
+            params = {"chat_id": chat_id, "photo": photo_link}
+            logging.info("")
             await session.get(url, params=params)
-            logging.info("send photo")
 
+    async def sendMessage(self, chat_id: int, session: aiohttp.ClientSession) -> None:
+        """reply as text from queries to bot if no picture"""
 
-async def main() -> None:
-    bot = PictureBot()
-    offset = -2
-    while True:
-        updates = await bot.getUpdates(offset)
-        if updates is not None:
-            offset, chat_id = await bot._parseUpdates(updates)
-            photo = await bot._get_picture()
-            await bot.sendPhoto(chat_id, photo)
-        await asyncio.sleep(1)
+        url = f"https://api.telegram.org/bot{self.__TOKEN}/sendMessage"
+        params = {"chat_id": chat_id, "text": "Error picture"}
+        logging.info("")
+        await session.get(url, params=params)
+
+    async def main(self) -> None:
+        while True:
+            async with aiohttp.ClientSession() as session:
+                updates = await self.getUpdates(session)
+                if updates:
+                    self.offset, chat_id = await self._parseUpdates(updates[0])
+                    await self.sendPhoto(chat_id, session)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    bot = PictureBot()
+    asyncio.run(bot.main())
---
+++ scraping/typed_python.py
@@ -0,0 +1,4 @@
+from typing import NamedTuple
+from collections import namedtuple
+
+User = namedtuple("User", "name,surname")
