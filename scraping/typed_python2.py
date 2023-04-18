4dfddd91a32468a2b76972c9b5b1d3752196329a
luculliano <vederecento@gmail.com>
2023-04-18 22:40:20 +0500

    add typed_python2

--- scraping/typed_python2.py
+++ scraping/typed_python2.py
@@ -1,7 +1,12 @@
 import asyncio
-from typing import NamedTuple
+from typing import Any, NamedTuple, TypeAlias
 
 import aiohttp
+import os
+
+TOKEN = os.getenv("OWM_TOKEN")
+
+Celcius: TypeAlias = float
 
 
 class Coordinates(NamedTuple):
@@ -9,23 +14,66 @@ class Coordinates(NamedTuple):
     latitude: float
 
 
+class Weather(NamedTuple):
+    temperature: Celcius
+    city: str
+    weather_type: str
+
+
+class CantGetCoordinates(Exception):
+    """Program can't get current coordinates"""
+
+
+class ApiServiceError(Exception):
+    """Program can't get current weather"""
+
+
 async def _parse_coordinates() -> Coordinates:
-    result = await get_coordinates()
-    loc = map(float, result["loc"].split(","))
+    ip_data = await _get_ip_data()
+    loc = map(float, ip_data["loc"].split(","))
     return Coordinates(*loc)
 
 
-async def get_coordinates() -> dict[str, str]:
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://ipinfo.io/json") as response:
-            return await response.json()
+async def _get_ip_data() -> dict[str, str]:
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://ipinfo.io/json", timeout=10) as response:
+                return await response.json()
+    except Exception:
+        raise CantGetCoordinates
+
+
+async def _get_openweather_data() -> dict[str, Any]:
+    try:
+        async with aiohttp.ClientSession() as session:
+            lat, lon = await _parse_coordinates()
+            params = {"appid": TOKEN, "lat": lat, "lon": lon, "units": "metric"}
+            async with session.get(
+                "https://api.openweathermap.org/data/2.5/weather",
+                params=params,
+                timeout=30,
+            ) as response:
+                return await response.json()
+    except Exception:
+        raise ApiServiceError
+
+
+async def _parse_weather() -> Weather:
+    weather_data = await _get_openweather_data()
+    temperature = weather_data["main"]["temp"]
+    city = weather_data["name"]
+    weather_type = weather_data["weather"][0]["description"]
+    return Weather(temperature, city, weather_type)
+
+
+async def get_weather() -> None:
+    temp, city, weather_type = await _parse_weather()
+    print(f"{city}: {temp}°C, {weather_type}")
 
 
 async def main() -> None:
-    task = asyncio.create_task(_parse_coordinates())
-    result = await asyncio.gather(task)
-    coordinates = result[0]
-    print(coordinates.longitude, coordinates.latitude, sep="\n")
+    task = asyncio.create_task(get_weather())
+    await asyncio.gather(task)
 
 
 if __name__ == "__main__":
