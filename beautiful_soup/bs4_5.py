6cb9e10ab453959a9be77320fd33b8b8cd2374ef
luculliano <vederecento@gmail.com>
2023-05-23 14:01:53 +0500

    finish first parser (bs4_5.py)

--- beautiful_soup/bs4_5.py
+++ beautiful_soup/bs4_5.py
@@ -1,14 +1,13 @@
 """scrap full https://parsinger.ru/"""
 
 # add progres bar
-# decrease uptime
-# add dataa to csv to make analysis
+# add data to csv to make analysis
 
 import asyncio
 from time import monotonic
 from typing import Iterator
 
-import aiohttp
+from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 import requests
 
@@ -17,62 +16,69 @@ URL = "https://parsinger.ru/html/index1_page_1.html"
 DOMAIN = "https://parsinger.ru/html/"
 
 
-def get_soup(url: str) -> BeautifulSoup:
-    response = requests.get(url)
-    return BeautifulSoup(response.content.decode(), "lxml")
+def get_category_links(url_source: str) -> Iterator[str]:
+    """it extracts all category links from source url, firstly"""
+    response = requests.get(url_source)
+    soup = BeautifulSoup(response.content.decode(), "lxml")
+    return (f"{DOMAIN}{i['href']}"
+            for i in soup.find("div", class_="nav_menu").find_all("a"))
 
 
-def get_category_links(url: str) -> Iterator[str]:
-    """it's used to extract all category links from source url"""
-    soup = get_soup(url)
-    return (
-        f"{DOMAIN}{i['href']}"
-        for i in soup.find("div", class_="nav_menu").find_all("a")
-    )
+async def get_pagen_links(url_category: str, session: ClientSession) -> Iterator[str]:
+    """it extracts all pagination links from category url, secondly"""
+    async with session.get(url_category) as response:
+        soup = BeautifulSoup(await response.text(encoding="utf-8"), "lxml")
+        return (f"{DOMAIN}{i['href']}"
+                for i in soup.find("div", class_="pagen").find_all("a"))
 
 
-def get_pagen_links(url: str) -> Iterator[str]:
-    """it's used to extract all pagination links from category url"""
-    soup = get_soup(url)
-    return (
-        f"{DOMAIN}{i['href']}"
-        for i in soup.find("div", class_="pagen").find_all("a")
-    )
+async def grab_pagen_links(session: ClientSession) -> Iterator[str]:
+    """grabs all pagen links to use them to extract card links, thirdly"""
+    tasks = (asyncio.create_task(get_pagen_links(i, session))
+             for i in get_category_links(URL))
+    result = await asyncio.gather(*tasks)
+    return (j for i in result for j in i)
 
 
-def get_card_links(url: str) -> Iterator[str]:
-    """it's used to extract all cards links from page url"""
-    soup = get_soup(url)
-    return (
-        f"{DOMAIN}{div.find('a')['href']}"
-        for div in soup.find_all("div", class_="sale_button")
-    )
+async def get_card_links(url_page: str, session: ClientSession) -> Iterator[str]:
+    """it extracts all cards links from page url, fourthly"""
+    async with session.get(url_page) as response:
+        soup = BeautifulSoup(await response.text(encoding="utf-8"), "lxml")
+        return (f"{DOMAIN}{div.find('a')['href']}"
+                for div in soup.find_all("div", class_="sale_button"))
 
 
-def grab_all_links() -> Iterator[str]:
-    urls_category = get_category_links(URL)
-    urls_page = (get_pagen_links(i) for i in urls_category)
-    urls_card = (get_card_links(j) for i in urls_page for j in i)
-    return (j for i in urls_card for j in i)
+async def grab_card_links(session: ClientSession) -> Iterator[str]:
+    """grabs all card links to use them to extract card info, fifthly"""
+    urls_page = await grab_pagen_links(session)
+    tasks = (asyncio.create_task(get_card_links(i, session)) for i in urls_page)
+    result = await asyncio.gather(*tasks)
+    return (j for i in result for j in i)
 
 
-async def get_good_revenue(url: str, session: aiohttp.ClientSession) -> int:
-    """it's used to extract revenue number from card url"""
-    async with session.get(url) as response:
+async def get_good_revenue(url_card: str, session: ClientSession) -> int:
+    """it's used to extract revenue number from card url, sixthly"""
+    async with session.get(url_card) as response:
         soup = BeautifulSoup(await response.text("utf-8"), "lxml")
         price = int(soup.find("span", id="price").text.split()[0])
         amount = int(soup.find("span", id="in_stock").text.split()[2])
         return price * amount
 
 
+async def get_cards_info(session: ClientSession) -> str:
+    """it combines each card result as last step"""
+    urls_card = await grab_card_links(session)
+    tasks = (asyncio.create_task(get_good_revenue(i, session)) for i in urls_card)
+    result = await asyncio.gather(*tasks)
+    return f"result: {sum(result)} rub"
+
+
 async def main() -> None:
-    async with aiohttp.ClientSession() as session:
-        tasks = (asyncio.create_task(get_good_revenue(i, session)) for i in grab_all_links())
-        revenue = await asyncio.gather(*tasks)
-        print("total revenue value:", sum(revenue), "rub")
+    start = monotonic()
+    async with ClientSession() as session:
+        print(await get_cards_info(session))
+    print("total uptime:", monotonic() - start)
 
 
 if __name__ == "__main__":
-    start = monotonic()
     asyncio.run(main())
-    print("total uptime:", monotonic() - start)
