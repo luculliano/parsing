1e6454efa4dd407cb2ac0fc75afcc22972a40239
luculliano <vederecento@gmail.com>
2023-05-24 13:09:10 +0500

    finish ajax_2.py

--- get_queries/ajax_2.py
+++ get_queries/ajax_2.py
@@ -1,4 +1,5 @@
 import csv
+from itertools import tee
 import json
 from pathlib import Path
 import sqlite3
@@ -12,17 +13,17 @@ url = "https://edtechs.ru/edtech_datatable/"
 store = Path(__file__).parent.joinpath(f"{Path(__file__).stem}_data")
 
 info = {
-    "1_2_q_2019": (1, 2),
-    "3_4_q_2019": (3, 4),
-    "1_2_q_2020": (5, 6),
-    "3_4_q_2020": (7, 8),
-    "total_2020_2021": (9, 10),
-    "1_2_q_2021": (11, 28),
-    "3_4_q_2021": (29, 30),
-    "1_2_q_2022": (32, 33),
-    "3_4_q_2022": (34, 35),
-    "total_2021_2022": (31, 36),
-    "1_1_q_2023": (37, 37),
+    "q_1_2019-q_2_2019": (1, 2),
+    "q_3_2019-q_4_2019": (3, 4),
+    "q_1_2020-q_2_2020": (5, 6),
+    "q_3_2020-q_4_2020": (7, 8),
+    "total_2020-total_2021": (9, 10),
+    "q_1_2021-q_2_2021": (11, 28),
+    "q_3_2021-q_4_2021": (29, 30),
+    "q_1_2022-q_2_2022": (32, 33),
+    "q_3_2022-q_4_2022": (34, 35),
+    "total_2021-total_2022": (31, 36),
+    "q_1_2023-q_1_2023": (37, 37),
 }
 
 columns = (
@@ -31,6 +32,28 @@ columns = (
     "foundation_year",
     "proceed_until",
     "proceed_from",
+    "direction_names",
+    "audience_names",
+    "owners",
+    "company_type_names",
+)
+
+columns_before = (
+    "name",
+    "activity",
+    "foundation_year",
+    "proceed_from",
+    "direction_names",
+    "audience_names",
+    "owners",
+    "company_type_names",
+)
+
+columns_after = (
+    "name",
+    "activity",
+    "foundation_year",
+    "proceed_until",
     "direction_names",
     "audience_names",
     "owners",
@@ -55,38 +78,48 @@ def save_json(name: str, data: dict) -> None:
         json.dump(data, file, ensure_ascii=False, indent=2)
 
 
-def save_as_csv(data, name: str) -> None:
+def save_as_csv(data: tuple[list[dict], list[dict]], name: str) -> None:
     if not store.exists(): store.mkdir()
-    with open(store.joinpath(f"{name}.csv"), "w", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, columns, quoting=csv.QUOTE_NONNUMERIC)
+    name_before, name_after = name.split("-")
+    before, after = data
+    with open(store.joinpath(f"{name_before}.csv"), "w", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, columns_before, quoting=csv.QUOTE_NONNUMERIC)
         writer.writeheader()
-        writer.writerows(data)
+        writer.writerows(before)
+    with open(store.joinpath(f"{name_after}.csv"), "w", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, columns_after, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writeheader()
+        writer.writerows(after)
 
 
-def parse_json(json_object: Any) -> list[dict[str, Any]]:
+def parse_json(json_object: Any) -> tuple[list[dict], list[dict]]:
     companies: list[dict[str, Any]] = json_object["data"]
-    new_loaded = []
+    companies_before, companies_after = [], []
     for company in companies:
         main_data = ((key, value) for key, value in company.items() if key in columns)
-        main_data = dict(map(lambda tpl: (tpl[0], tpl[1][0])
-                             if tpl[0] == "proceed_until" else tpl, main_data))
-        new_loaded.append(main_data)
-    return new_loaded
+        main_data = tee(map(lambda tpl: (tpl[0], tpl[1][0])
+                        if tpl[0] == "proceed_until" else tpl, main_data))
+        before = dict((key, value) for key, value in main_data[0] if key != "proceed_until")
+        after = dict((key, value) for key, value in main_data[1] if key != "proceed_from")
+        companies_before.append(before)
+        companies_after.append(after)
+    return companies_before, companies_after
 
 
 def save_data_in_db() -> None:
     con = sqlite3.connect(store.joinpath(f"{Path(__file__).stem}_data.db"))
     for csv_file in store.iterdir():
         if csv_file.suffix == ".csv":
-            con.execute(f"create table if not exists t{csv_file.stem}(name text, activity text, "
-                         "foundation_year int, proceed_until real, proceed_from real, "
+            con.execute(f"create table if not exists {csv_file.stem}(name text, "
+                         "activity text, foundation_year int, revenue real, "
                          "direction_names text, audience_names text, owners text, "
                          "company_type_names text)")
             file = open(csv_file, encoding="utf-8")
             loaded = csv.reader(file)
             next(loaded)
             for line in loaded:
-                con.execute(f"insert into t{csv_file.stem} values(?, ?, ?, ?, ?, ?, ?, ?, ?)", line)
+                con.execute(f"insert into {csv_file.stem} "
+                             "values(?, ?, ?, ?, ?, ?, ?, ?)", line)
             file.close()
     con.commit()
     con.close()
